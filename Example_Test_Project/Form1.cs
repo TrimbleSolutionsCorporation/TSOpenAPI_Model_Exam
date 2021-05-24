05feed03eb7298bd97dc6d7bec18ec10300bd0fa
cahe <carlos.herrero@trimble.com>
2021-05-24 09:19:49 +0300

    Test Project to run before Exam starts

---
+++ Example_Test_Project/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Windows.Forms;
+using Tekla.Structures.Model;
+
+namespace Example_Test_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonTest_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ModelInfo mi = new Model().GetInfo();
+
+                if (!mi.ModelName.Contains("API_Developer_Exam"))
+                {
+                    CloseTestAppNotConnected();
+                    return;
+                }
+
+                var moe = new Model().GetModelObjectSelector().GetAllObjectsWithType(ModelObject.ModelObjectEnum.BEAM);
+
+                if (moe.GetSize() < 10)
+                {
+                    CloseTestAppNotConnected();
+                    return;
+                }
+
+                label1.Text = "Project is connected with Exam model.";
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                button4.Enabled = true;
+            }
+            catch
+            {
+                CloseTestAppNotConnected();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Model myModel = new Model();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Model myModel = new Model();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Model myModel = new Model();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Model myModel = new Model();
+        }
+
+        void CloseTestAppNotConnected()
+        {
+            string errorMessage = "Project cannot connect with exam model.";
+            label1.Text = errorMessage;
+
+            buttonTest.Enabled = false;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+
+            return;
+        }
+    }
+}
